fbadf7d6be1e8c4f7acf01393eb15a4e48598de4
Dejan Pekez <dejan@pekez.de>
2018-05-02 12:42:34 +0200

    implemented test class for XY

---
+++ src/core/Test_XY.java
@@ -0,0 +1,23 @@
+package core;
+
+import entities.Entity;
+
+/**
+ * Prints every single entity and its location.
+ * Intended for to check for equally located entities.
+ */
+
+
+public class Test_XY {
+    public static void main(String[] args) {
+        Board board = new Board(new BoardConfig());
+
+        System.out.println(board);
+
+        for(Entity entity: board.getEntities())
+            if(entity != null)
+                System.out.println(entity);
+
+    }
+
+}
