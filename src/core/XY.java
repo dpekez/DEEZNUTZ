147033bc1780efbc09d508fa96b087ef5973947c
Dejan Pekez <dejan@pekez.de>
2018-05-18 19:02:10 +0200

    reverted to old XY.class

--- src/core/XY.java
+++ src/core/XY.java
@@ -9,6 +9,7 @@ public final class XY {
     private final int x;
     private final int y;
 
+
     public XY(int x, int y) {
         this.x = x;
         this.y = y;
@@ -26,17 +27,32 @@ public final class XY {
         return new XY(x + vector.x, y + vector.y);
     }
 
+
+    /**
+     * Generates a random move vector, skips zero vectors (0, 0).
+     * Generates vectors like  1, -1
+     *                        -1,  0
+     *                         0,  1
+     *                         1,  1
+     *                         ...
+     *
+     * @return  the generated vector
+     */
+
     public static XY generateRandomMoveVector() {
         Random random = new Random();
         int x, y;
-        x = random.nextInt(3) - 1;
-        y = random.nextInt(3) - 1;
-        while (x == 0 && y == 0) {
+
+        do {
+
             x = random.nextInt(3) - 1;
             y = random.nextInt(3) - 1;
-        }
+
+        } while(x == 0 && y == 0);
+
         return new XY(x, y);
     }
+
 
     /**
      * Generates and returns a random and empty location inside of the
@@ -44,32 +60,34 @@ public final class XY {
      *
      * @param boardSize the board size
      * @param entities  an array of every entity currently on the board
-     * @return the random location
+     * @return          the random location
      */
 
-    static XY generateRandomLocation(XY boardSize, Entity[] entities) {
+    public static XY generateRandomLocation(XY boardSize, Entity[] entities) {
         Random random = new Random();
-        boolean isNotEmpty = false;
-        int randomX = random.nextInt(boardSize.getX() - 2) + 1;
-        int randomY = random.nextInt(boardSize.getY() - 2) + 1;
-        XY xy = new XY(randomX, randomY);
-        for (Entity entity : entities)
-            if (entity != null && entity.getLocation().getX() == xy.x && entity.getLocation().getY() == xy.y) {
-                isNotEmpty = true;
-            }
-        while (isNotEmpty) {
+        boolean isNotEmpty;
+        XY xy;
+
+        do {
+
             isNotEmpty = false;
-            randomX = random.nextInt(boardSize.getX() - 2) + 1;
-            randomY = random.nextInt(boardSize.getY() - 2) + 1;
+
+            int randomX = random.nextInt(boardSize.getX() - 2) + 1;
+            int randomY = random.nextInt(boardSize.getY() - 2) + 1;
+
             xy = new XY(randomX, randomY);
-            for (Entity entity : entities)
-                if (entity != null && entity.getLocation().getX() == xy.x &&
+
+            for(Entity entity: entities)
+                if(entity != null && entity.getLocation().getX() == xy.x &&
                         entity.getLocation().getY() == xy.y) {
                     isNotEmpty = true;
                 }
-        }
+
+        } while(isNotEmpty);
+
         return xy;
     }
+
 
     /**
      * Calculates the distance between two locations.
@@ -77,12 +95,15 @@ public final class XY {
      * @param xy Position of Comparision
      * @return the length of the XY Object to the other one
      */
-    double distanceFrom(XY xy) {
+
+    public double distanceFrom(XY xy) {
         return Math.sqrt(Math.pow(xy.getX() - x, 2) + Math.pow(xy.getY() - y, 2));
     }
+
 
     @Override
     public String toString() {
         return "x: " + x + ", y: " + y;
     }
+
 }
